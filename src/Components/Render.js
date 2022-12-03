fa5421a6ef57394ec46e44137b33b7a88064817f
Adelakun <adelakunabayomi123@gmail.com>
2022-12-03 14:20:51 +0100

    mainwork

--- src/Components/Render.js
+++ src/Components/Render.js
@@ -2,12 +2,13 @@ import React, { useEffect, useState } from 'react'
 import '../Styles/Render.css'
 import ArrowDown from '../Assets/ArrowDown.png'
 import ArrowUp from '../Assets/ArrowUp.png'
+import ContentLoader from "react-content-loader"
 
 
 
 
 
-const Render = () => {
+const Render = (props) => {
     const [data, setData] = useState([])
     const [isLoading, setIsLoading] = useState(true)
 
@@ -17,13 +18,89 @@ const Render = () => {
             fetch('https://api.coinlore.net/api/tickers/?start=0&limit=5')
                 .then((res) => res.json())
                 .then((rep) => setData(rep.data), setIsLoading(false))
-        }, 3000)
+        }, 5000)
 
     })
     if (isLoading) {
         return (
             <div className='itsloading1'>
-                <p className='loading animate__animated animate__pulse animate__slower	3s animate__infinite	infinite' >Loading, please wait...</p>
+                <div className="subloading11">
+                    <div className="subloader12">
+                        <ContentLoader
+                            speed={2}
+                            width={500}
+                            height={160}
+                            viewBox="50 0 400 130"
+                            backgroundColor="#d9d9d9"
+                            foregroundColor="#ededed"
+                            {...props}
+                        >
+                            <rect x="50" y="6" rx="4" ry="4" width="343" height="50" />
+                            <rect x="50" y="55" rx="4" ry="4" width="343" height="50" />
+                            <rect x="50" y="104" rx="4" ry="4" width="343" height="50" />
+                        </ContentLoader>
+                    </div>
+                    <div className="subloader12">
+                        <ContentLoader
+                            speed={2}
+                            width={500}
+                            height={160}
+                            viewBox="50 0 400 130"
+                            backgroundColor="#d9d9d9"
+                            foregroundColor="#ededed"
+                            {...props}
+                        >
+                            <rect x="50" y="6" rx="4" ry="4" width="343" height="50" />
+                            <rect x="50" y="55" rx="4" ry="4" width="343" height="50" />
+                            <rect x="50" y="104" rx="4" ry="4" width="343" height="50" />
+                        </ContentLoader>
+                    </div>
+                    <div className="subloader12">
+                        <ContentLoader
+                            speed={2}
+                            width={500}
+                            height={160}
+                            viewBox="50 0 400 130"
+                            backgroundColor="#d9d9d9"
+                            foregroundColor="#ededed"
+                            {...props}
+                        >
+                            <rect x="50" y="6" rx="4" ry="4" width="343" height="50" />
+                            <rect x="50" y="55" rx="4" ry="4" width="343" height="50" />
+                            <rect x="50" y="104" rx="4" ry="4" width="343" height="50" />
+                        </ContentLoader>
+                    </div>
+                    <div className="subloader12">
+                        <ContentLoader
+                            speed={2}
+                            width={500}
+                            height={160}
+                            viewBox="50 0 400 130"
+                            backgroundColor="#d9d9d9"
+                            foregroundColor="#ededed"
+                            {...props}
+                        >
+                            <rect x="50" y="6" rx="4" ry="4" width="343" height="50" />
+                            <rect x="50" y="55" rx="4" ry="4" width="343" height="50" />
+                            <rect x="50" y="104" rx="4" ry="4" width="343" height="50" />
+                        </ContentLoader>
+                    </div>
+                    <div className="subloader12">
+                        <ContentLoader
+                            speed={2}
+                            width={500}
+                            height={160}
+                            viewBox="50 0 400 130"
+                            backgroundColor="#d9d9d9"
+                            foregroundColor="#ededed"
+                            {...props}
+                        >
+                            <rect x="50" y="6" rx="4" ry="4" width="343" height="50" />
+                            <rect x="50" y="55" rx="4" ry="4" width="343" height="50" />
+                            <rect x="50" y="104" rx="4" ry="4" width="343" height="50" />
+                        </ContentLoader>
+                    </div>
+                </div>
             </div>
         )
     }
